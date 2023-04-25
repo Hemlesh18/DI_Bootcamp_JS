c1919447c13842c9918392f28e6326ebaa8a7560
Hemlesh18 <hemlesh@icloud.com>
2023-04-25 23:41:53 +0400

    day2 daily challenge

--- week2/day1/exercise/xp/index.js
+++ week2/day1/exercise/xp/index.js
@@ -12,6 +12,7 @@ console.log(myWatchedSeriesLength)
 // 2
 const myWatchedSeriesSentence = myWatchedSeries.splice(2,1,"and the big bang theory");
 console.log(myWatchedSeriesSentence)
+console.
 // 3
 console.log("i watched 3 series :" + myWatchedSeriesSentence)
 
--- week2/day2/dailyChallege/index.js
+++ week2/day2/dailyChallege/index.js
@@ -3,7 +3,23 @@ const wordNotPosition = sentence.indexOf("not");
 console.log(wordNotPosition);
 const wordsBadPosition = sentence.indexOf("bad")+3
 console.log(wordsBadPosition);
-let result = sentence.substring(wordNotPosition,wordsBadPosition)
-console.log(result);
-let newSentence = sentence.replace(result,"good")
-console.log(newSentence);
+if (wordNotPosition < wordsBadPosition){
+    let result = sentence.substring(wordNotPosition,wordsBadPosition)
+    let newSentence = sentence.replace(result,"good")
+    console.log(newSentence);
+
+}
+else{
+    console.log(sentence);
+}
+
+// method2
+// let sentence1 = "The movie is not that bad, I like it";
+// let wordNot = sentence1.indexOf("not");
+// let wordBad = sentence1.indexOf("bad");
+
+// if (wordNot !== -1 && wordBad !== -1 && wordNot < wordBad) {
+//   sentence1 = sentence1.slice(0, wordNot) + "good" + sentence1.slice(wordBad + 3);
+// }
+
+// console.log(sentence1);
