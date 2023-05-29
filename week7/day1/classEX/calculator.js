e23910c0f3ed15625c70736f8de07b505a470cee
Hemlesh18 <hemlesh@icloud.com>
2023-05-29 15:37:57 +0400

    hiii

---
+++ week7/day1/classEX/calculator.js
@@ -0,0 +1,3 @@
+const operator = require("./operation.js")
+console.log(operator.addoperator(4,10));
+console.log(operator.divoperator(88 ,10));
---
+++ week7/day1/classEX/operation.js
@@ -0,0 +1,11 @@
+function addoperator(x,y){
+    return x + y
+};
+
+let divoperator = (x,y)=>{
+    return x / y
+};
+module.exports={
+    addoperator:addoperator,
+    divoperator:divoperator
+}
